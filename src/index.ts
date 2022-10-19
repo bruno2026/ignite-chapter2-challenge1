5a90a8ae83423a4bbf5e600a6a8e1e4118b36a67
Bruno Gomes <brunoo.gomes11@gmail.com>
2022-10-19 19:34:06 -0300

    Implementation chalenge 1 chapter 2

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+import cors from "cors";
 import express from "express";
 
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/users", usersRoutes);
