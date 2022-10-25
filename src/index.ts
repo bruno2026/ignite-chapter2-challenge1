03c8e098edfc4ba529574e898922e40e9c09e4e4
Bruno Gomes <brunoo.gomes11@gmail.com>
2022-10-24 22:50:57 -0300

    Install Swagger and documentation first route create user

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import cors from "cors";
 import express from "express";
+import swaggerUI from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use("/users", usersRoutes);
 
